a5b04c4c7e44346a3a8035382a359e290cab159a
mnog1992 <115990262+mnog1992@users.noreply.github.com>
2022-10-18 13:07:38 +0100

    Update streamlit_app.py
    corrects typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from urllib.error import URLError
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur: 
-    my_cur.execute("insert into fruit_load_list values(new_fruit)")
+    my_cur.execute("insert into fruit_load_list values('" + new_fruit + "')")
     return "Thanks for adding" + new_fruit
 
 
